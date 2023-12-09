3833118be7c78072197def8d9f7d07446ced60f8
Alex Clark <aclark@aclark.net>
2023-12-09 18:43:05 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 // App.js
 import React from 'react';
-import { MyContextProvider, useMyContext } from './MyContext';
 
 const App = () => {
-  const { exampleValue, updateValue } = useMyContext();
 
   return (
-    <MyContextProvider>
       <div>
         <p>Hello, world!</p>
-        <p>Context Value: {exampleValue}</p>
-        <button onClick={() => updateValue('New Value')}>Update Context Value</button>
       </div>
-    </MyContextProvider>
   );
 };
 
